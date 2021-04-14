1089b20bba332377abde9cb1ac4eb52f8a58fd52
Onur Erbil <o.erbil@divine.de>
2021-04-14 17:01:35 +0200

    changed nav to sticky

---
+++ script/app.js
@@ -0,0 +1,22 @@
+const sticky =  document.getElementById("sticky");
+const links = sticky.querySelectorAll("a");
+const logo = document.getElementById("logo-img");
+
+window.onscroll = function() {myFunction()};
+// change bg-color, color and logo if you scroll down
+function myFunction() {
+  if (document.documentElement.scrollTop > 5) {
+        sticky.className = "add-background";
+        logo.src = "./img/Logo_Abaayo.svg";
+        links.forEach(link => {
+            link.className = "linkColor";
+        });
+  } else {
+        sticky.className = "sticky";
+        logo.src = "./img/Logo_Abaayo_white.svg";
+        links.forEach(link => {
+            link.className = "";
+        });
+  }
+}
+
